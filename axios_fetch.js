b8aca7e8c6623df3b3e51629126a78ddd6c23e56
Peter <111876456+AY0UBYOUSFI@users.noreply.github.com>
2025-09-14 00:26:56 +0100

    Add files via upload

---
+++ axios_fetch.js
@@ -0,0 +1,22 @@
+// import axios from "./node_modules/axios/dist/esm/axios.js";
+
+// axios.get('https://tarmeezacademy.com/api/v1/users')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+
+function image_testing(image){
+
+  let image_group = [image+'.jpg',image+'.webp',image+'.avif',image+'.jpeg'];
+
+}
+
+
+
+
+
